ed6cb8c327a33248fe5f9c2a89b2610f5b2420ae
Matt <contact@maleero.com>
2021-04-30 15:39:06 -0500

    Update Controller.ts

--- src/attributes/Controller.ts
+++ src/attributes/Controller.ts
@@ -2,12 +2,24 @@ import {Scope} from "../Scope";
 import {Attribute} from "../Attribute";
 
 export class Controller extends Attribute {
+    protected controllerClassName: string;
+
     public setup(): void {
         const parentScope: Scope = this.tag.parent.scope;
         if (!parentScope)
             return;
-        const controllerClassName: string = this.tag.rawAttributes['v-class'];
-        const cls: any = window[controllerClassName];
-        this.tag.scope.wrap(new cls());
+        this.controllerClassName = this.tag.rawAttributes['v-class'];
+        const cls: any = window[this.controllerClassName];
+
+        if (!!cls) {
+            this.registerController(cls)
+        } else {
+            console.log('waiting for class to be registered');
+            window['vision'].once(`register:${this.controllerClassName}`, this.registerController.bind(this));
+        }
+    }
+
+    protected registerController(cls) {
+        this.tag.wrap(cls);
     }
 }
