a22e502dc17bf3946266a04af686b78f6183954d
Matt <contact@maleero.com>
2021-05-11 14:23:34 -0500

    Update Attribute.ts

--- src/Attribute.ts
+++ src/Attribute.ts
@@ -7,6 +7,15 @@ export abstract class Attribute {
         this.configure();
     }
 
-    public setup(): void {};
-    protected configure(): void {};
+    protected async configure() {};
+    public async setup() {};
+    public async execute() {};
+
+    public getAttributeValue(key: string, index: number = 0, fallback: any = null) {
+        return this.tag.getRawAttributeValue(key, index, fallback);
+    }
+
+    public getAttributeValues(key: string, fallback: any = null) {
+        return this.tag.rawAttributes[key] || fallback;
+    }
 }
