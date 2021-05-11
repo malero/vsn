85e2f8ee730e805419933683dc5c36aa722f4e35
Matt <contact@maleero.com>
2021-05-11 14:25:47 -0500

    Update Vision.ts

--- src/Vision.ts
+++ src/Vision.ts
@@ -1,7 +1,10 @@
 import {DOM} from "./DOM";
 import {EventDispatcher} from "simple-ts-event-dispatcher";
+import {WrappedArray} from "./Scope";
+import {IDeferred, IPromise, Promise} from "simple-ts-promise";
 
 export class Vision extends EventDispatcher {
+    protected static _instance: Vision;
     protected dom?: DOM;
     protected controllers: {[key: string]: any} = {};
 
@@ -11,18 +14,41 @@ export class Vision extends EventDispatcher {
             "DOMContentLoaded",
             this.setup.bind(this)
         );
+        this.registerClass(WrappedArray, 'WrappedArray');
     }
 
     setup(): void {
         this.dom = new DOM(document);
     }
 
-    registerClass(cls: any) {
-        const key: string = cls.prototype.constructor.name;
-        this.controllers[cls.prototype.constructor.name] = cls;
+    registerClass(cls: any, constructorName: string = null) {
+        const key: string = constructorName || cls.prototype.constructor.name;
+        this.controllers[key] = cls;
         this.trigger(`registered:${key}`, cls);
+    }
+
+    getClass(key: string): IPromise<any> {
+        const deferred: IDeferred<any> = Promise.defer();
+
+        if (!!this.controllers[key]) {
+            deferred.resolve(this.controllers[key]);
+        } else {
+            this.once(`registered:${key}`, (cls) => {
+                deferred.resolve(cls);
+            })
+        }
+
+        return deferred.promise;
+    }
+
+    public static get instance() {
+        if (!Vision._instance)
+            Vision._instance = new Vision();
+
+        return Vision._instance;
     }
 }
 
-export const vision: Vision = new Vision();
+export const vision: Vision = Vision.instance;
+window['Vision'] = Vision;
 window['vision'] = vision;
