666cdd79561a0d3743cbbf9a961743c00563dea2
Matt <contact@maleero.com>
2021-06-11 16:20:19 -0500

    tests

--- test/attributes/ListItem.spec..ts
+++ test/attributes/ListItem.spec..ts
@@ -1,8 +1,23 @@
 import {DOM} from "../../src/DOM";
 import {ListItem} from "../../src/attributes/ListItem";
 import {vision} from "../../src/Vision";
+import {List} from "../../types/attributes/List";
+import {WrappedArray} from "../../types/Scope";
 
-class TestItem {}
+class TestItem {
+    test: number = null;
+
+    constructor(
+        public readonly v: number = 123,
+        test: number
+    ) {
+        this.test = test;
+    }
+
+    getValue() {
+        return this.v
+    }
+}
 
 describe('ListItem', () => {
     it("vsn-list-item should find it's parent list or complain", async () => {
@@ -34,6 +49,32 @@ describe('ListItem', () => {
 
             expect(listItemAttr.list).toBe(list);
             done();
+        });
+    });
+
+    it("should properly wrap list item class", (done) => {
+        document.body.innerHTML = `
+            <ul vsn-list:list id="test">
+                <li vsn-list-item:item="TestItem"></li>
+            </ul>
+        `;
+        vision.registerClass(TestItem, 'TestItem');
+
+        const dom = new DOM(document);
+        dom.once('built', async () => {
+            const list = await dom.getTagForElement(document.getElementById('test'));
+            const listAttr: List = list.getAttribute('vsn-list') as List;
+            (listAttr.items as WrappedArray<TestItem>).bind('add', () => {
+                const listItem = listAttr.tags[0];
+
+                expect(listItem.scope.wrapped instanceof TestItem).toBe(true);
+                expect(listItem.scope.get('test')).toBe(555);
+                expect(listItem.scope.get('v')).toBe(1);
+                done();
+            })
+
+            listAttr.items.length = 0;
+            listAttr.items.push(new TestItem(555, 1));
         });
     });
 });
