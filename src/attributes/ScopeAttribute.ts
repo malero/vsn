adbfe56d7e67494d1d06c12b0984b31f836fbfcb
malero <contact@maleero.com>
2022-05-13 10:00:19 -0500

    Allow setting scope data with vsn-scope

--- src/attributes/ScopeAttribute.ts
+++ src/attributes/ScopeAttribute.ts
@@ -1,8 +1,27 @@
 import {Attribute} from "../Attribute";
 import {Registry} from "../Registry";
+import {Tree} from "../AST";
+import {Scope} from "../Scope";
 
 @Registry.attribute('vsn-scope')
 export class ScopeAttribute extends Attribute {
     public static readonly canDefer: boolean = false;
     public static readonly scoped: boolean = true;
+    protected tree: Tree;
+
+    public async compile() {
+        this.tree = new Tree(this.getAttributeValue());
+        await this.tree.prepare(this.tag.scope, this.tag.dom, this.tag);
+        await super.compile();
+    }
+
+    public async extract() {
+        const value = await this.tree.evaluate(this.tag.scope, this.tag.dom, this.tag);
+        if (!(value instanceof Scope)) {
+            throw new Error(`Scope value must be an object, got ${typeof value}`);
+        }
+        for (const key of value.data.keys) {
+            this.tag.scope.set(key, value.data[key]);
+        }
+    }
 }
---
+++ test/attributes/ScopeAttribute.spec.ts
@@ -0,0 +1,23 @@
+import {DOM} from "../../src/DOM";
+import "../../src/Types";
+import "../../src/attributes/_imports";
+
+
+describe('ScopeAttribute', () => {
+    it("vsn-styles to just work", (done) => {
+        document.body.innerHTML = `
+            <div vsn-scope="{'asd':123, 'sdf': 'asd'}">
+                <span vsn-bind="asd"></span>
+            </div>
+        `;
+        const dom = new DOM(document);
+        dom.once('built', async () => {
+            const element = (await dom.exec('?(div)'))[0];
+            expect(element.scope.get('asd')).toBe(123);
+            expect(element.scope.get('sdf')).toBe('asd');
+            const span = (await element.exec('?>(:first-child)'))[0];
+            expect(span.element.innerText).toBe('123');
+            done();
+        });
+    });
+});
